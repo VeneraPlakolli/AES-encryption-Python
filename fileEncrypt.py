f38b9871e6a77e4cd807cb4307ab99f886bebd9f
VeneraPlakolli <94639395+VeneraPlakolli@users.noreply.github.com>
2023-01-07 13:17:29 +0100

    File-encrypted

---
+++ fileEncrypt.py
@@ -0,0 +1,23 @@
+from Crypto.Cipher import AES 
+from Crypto.Util.Padding import pad
+from base64 import b64encode
+
+key = input('Please insert your password: ')
+key = key.encode('UTF-8')
+key = pad(key, AES.block_size)
+
+def encrypt(filename, key):
+    with open(filename, 'rb') as entry:
+        data = entry.read()
+        cipher = AES.new(key, AES.MODE_CFB)
+        ciphertext = cipher.encrypt(pad(data,AES.block_size))
+        iv = b64encode(cipher.iv).decode('UTF-8')
+        ciphertext = b64encode(ciphertext).decode('UTF-8')
+        to_write = iv + ciphertext
+    entry.close()
+
+    with open(filename+'.enc','w') as data:
+        data.write(to_write)
+    data.close()
+
+encrypt('test.txt', key)
