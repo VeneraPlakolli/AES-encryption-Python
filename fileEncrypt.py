87d245fffe04a8d8949c263a9339fdcf0891cbbd
VeneraPlakolli <94639395+VeneraPlakolli@users.noreply.github.com>
2023-01-07 15:46:14 +0100

    file encryption through the terminal

--- fileEncrypt.py
+++ fileEncrypt.py
@@ -1,13 +1,15 @@
 from Crypto.Cipher import AES 
 from Crypto.Util.Padding import pad
+from Crypto.Random import get_random_bytes
 from base64 import b64encode
+
 
 key = input('Please insert your password: ')
 key = key.encode('UTF-8')
 key = pad(key, AES.block_size)
 
 def encrypt(filename, key):
-    with open(filename, 'rb') as entry:
+    with open(filename, 'rb') as entry:  
         data = entry.read()
         cipher = AES.new(key, AES.MODE_CFB)
         ciphertext = cipher.encrypt(pad(data,AES.block_size))
@@ -15,9 +17,12 @@ def encrypt(filename, key):
         ciphertext = b64encode(ciphertext).decode('UTF-8')
         to_write = iv + ciphertext
     entry.close()
+    return to_write
 
-    with open(filename+'.enc','w') as data:
-        data.write(to_write)
-    data.close()
+file_name=input("Shkruni emrin e file qe doni te enkriptoni : ")
 
-encrypt('test.txt', key)
+encrypted_file = encrypt(file_name, key)
+
+
+with open(file_name+".enc", 'w') as f:   
+    f.write(encrypted_file)
