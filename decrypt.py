372ccc33d8b263086fb5a4a9435e695244ea75b4
Puhiza Tmava <94013520+PuhizaTmava@users.noreply.github.com>
2023-01-08 13:42:23 +0100

    Update decrypt.py
    changes

--- decrypt.py
+++ decrypt.py
@@ -4,7 +4,7 @@ from base64 import b64decode
 import getpass
 
 
-key = getpass.getpass('Please enter your password: ')
+key = getpass.getpass('Shkruaje password: ')
 key = key.encode('UTF-8')
 key = pad(key,AES.block_size)
 
@@ -17,12 +17,12 @@ with open('test.txt.enc', 'r') as entry:  # could be 'rb'
         iv = b64decode(iv)
         ciphertext = data[24:length]
         ciphertext = b64decode(ciphertext)
-        cipher = AES.new(key,AES.MODE_CFB,iv)
+        cipher = AES.new(key,AES.MODE_CBC,iv)
         decrypted = cipher.decrypt(ciphertext)
         decrypted = unpad(decrypted,AES.block_size)
         with open('test.txt.dec','wb') as data:   # only 'wb' not 'w'
             data.write(decrypted)
         data.close()
-        print("Decrypted file is in your folder!")
+        print("File i dekriptuar eshte ne folder!")
     except(ValueError,KeyError):
-        print('wrong password')
+        print('password-i gabim')
