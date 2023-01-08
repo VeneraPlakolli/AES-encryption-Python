36bda7559a2715aa8bf71d479f7c700d9f26e5dc
VeneraPlakolli <94639395+VeneraPlakolli@users.noreply.github.com>
2023-01-08 12:28:38 +0100

    some changes/comments

--- fileEncrypt.py
+++ fileEncrypt.py
@@ -10,18 +10,21 @@ key = pad(key, AES.block_size)
 def encrypt(filename, key):
     with open(filename, 'rb') as entry:  
         data = entry.read()
-        cipher = AES.new(key, AES.MODE_CFB)
+        cipher = AES.new(key, AES.MODE_CBC)  
         ciphertext = cipher.encrypt(pad(data,AES.block_size))
         iv = b64encode(cipher.iv).decode('UTF-8')
         ciphertext = b64encode(ciphertext).decode('UTF-8')
         to_write = iv + ciphertext
     entry.close()
     return to_write
+    
 
 file_name=input("Shkruni emrin e file qe doni te enkriptoni : ")
+print("Fajllin e enkriptuar e gjeni ne folderin tuaj!")
 
+#Initializing the variable with encryption function result
 encrypted_file = encrypt(file_name, key)
 
-
+#Writing the text/image to a file
 with open(file_name+".enc", 'w') as f:   
     f.write(encrypted_file)
