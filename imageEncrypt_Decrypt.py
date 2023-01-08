f8d2eaa4ef170cc8e191240cf41cfe29dc15071a
Puhiza Tmava <94013520+PuhizaTmava@users.noreply.github.com>
2023-01-08 13:44:33 +0100

    Update imageEncrypt_Decrypt.py
    add

--- imageEncrypt_Decrypt.py
+++ imageEncrypt_Decrypt.py
@@ -10,8 +10,22 @@ def Encrypt(filename, key):
     file = open("ENC-"+filename, 'wb')
     file.write(data)
     file.close()
+   
+
+def Decrypt(filename, key):
+    file = open(filename, 'rb')
+    data = file.read()
+    file.close()
+
+    data = bytearray(data)
+    for index, value in enumerate(data):
+        data[index] = value ^ key
+
+    file = open(filename, 'wb')
+    file.write(data)
+    file.close()
 
 
 key = int(input("Shkruani nje celes ne intervalin 1-255: "))
-filename = input("Please enter a filename: ")
+filename = input("Shkruani emrin e file: ")
 Encrypt(filename, key)
